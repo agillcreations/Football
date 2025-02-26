4290df27cf1e31d36df4b8e5ae8fda7630053aab
agmonster08 <26757790+agmonster08@users.noreply.github.com>
2025-02-25 22:16:33 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Arismendy Gill on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
